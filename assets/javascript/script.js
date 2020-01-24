1f82b08302823d003200d8a39a6ccb4d14f18c61
Andrew Lin <andrewlin618@gmail.com>
2020-01-24 09:51:06 -0600

    add launch page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,30 @@
+
+
+
+
 $( "#btn1" ).click(function() {
+    $("#btn1").css("color","orange");
+    $("#btn2").css("color","white");
+    $("#btn3").css("color","white");
+    $("#launch-page").addClass('d-none');
     $("#about-me-page").removeClass('d-none');
     $("#portfolio-page").addClass('d-none');
     $("#contact-page").addClass('d-none');
 });
 $( "#btn2" ).click(function() {
+    $("#btn1").css("color","white");
+    $("#btn2").css("color","orange");
+    $("#btn3").css("color","white");
+    $("#launch-page").addClass('d-none');
     $("#about-me-page").addClass('d-none');
     $("#portfolio-page").removeClass('d-none');
     $("#contact-page").addClass('d-none');
 });
 $( "#btn3" ).click(function() {
+    $("#btn1").css("color","white");
+    $("#btn2").css("color","white");
+    $("#btn3").css("color","orange");
+    $("#launch-page").addClass('d-none');
     $("#about-me-page").addClass('d-none');
     $("#portfolio-page").addClass('d-none');
     $("#contact-page").removeClass('d-none');
